9807da5300296028b58df8f386684266647e3acd
Sejal2407 <sejalokhatri@gmail.com>
2026-04-24 16:30:29 +0530

    updated backend url

--- Frontend/frontend-qr/src/Components/QrForm.jsx
+++ Frontend/frontend-qr/src/Components/QrForm.jsx
@@ -23,7 +23,7 @@ function QrForm() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/generate", {
+      const response = await fetch("https://qr-generator-xk3h.onrender.com/api/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
